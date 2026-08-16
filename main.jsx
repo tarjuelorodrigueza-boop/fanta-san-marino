873d550ee01f059b231e112d2e8b3bb2091a7550
tarjuelorodrigueza-boop <tarjuelorodrigueza@gmail.com>
2026-08-16 18:01:13 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+import "./styles.css";import { createClient } from "@supabase/supabase-js";
 
 const OFFICIAL =
   "https://www.fsgc.sm/it/campionato/campionato-sammarinese/1";
